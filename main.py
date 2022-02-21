c41426d315503620682bb28a2889b001e2484165
kath <katya.permyakova.2004@mail.ru>
2022-02-21 13:10:03 +0300

    Список профессий

--- main.py
+++ main.py
@@ -14,5 +14,10 @@ def training(prof):
     return render_template('engineer.html', title='Name', prof=prof)
 
 
+@app.route('/list_prof/<type_list>')
+def list_prof(type_list):
+    return render_template('list_prof.html', title='Name', type_list=type_list)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
