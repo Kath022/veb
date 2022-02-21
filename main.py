6c4b9ecdb7cb47e1fe65f5b9cdad16bbcca2c07d
kath <katya.permyakova.2004@mail.ru>
2022-02-21 14:27:10 +0300

    Автоматический ответ

--- main.py
+++ main.py
@@ -19,5 +19,23 @@ def list_prof(type_list):
     return render_template('list_prof.html', title='Name', type_list=type_list)
 
 
+autor_answer_data = {'title': 'title',
+    'surname': 'surname',
+    'name': 'name',
+    'education': 'education',
+    'profession': 'profession',
+    'sex': 'sex',
+    'motivation': 'motivation',
+    'ready': 'ready',
+}
+
+
+# ИЗ КАКОГО ОБРАБОТЧИКА БЕРУТЬСЯ ДАННЫЕ????
+@ app.route('/answer')
+@ app.route('/auto_answer')
+def auto_answer():
+    return render_template('auto_answer.html', title=autor_answer_data['title'], autor_answer_data=autor_answer_data)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
