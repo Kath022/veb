147a7d96fa31515392dd7c1eb2f04ab65e8583b8
kath <katya.permyakova.2004@mail.ru>
2022-02-23 22:01:34 +0300

    Двойная защита

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route('/<title>')
 @app.route('/index/<title>')
@@ -30,11 +33,27 @@ autor_answer_data = {'title': 'title',
 }
 
 
-# ИЗ КАКОГО ОБРАБОТЧИКА БЕРУТЬСЯ ДАННЫЕ????
 @ app.route('/answer')
 @ app.route('/auto_answer')
 def auto_answer():
     return render_template('auto_answer.html', title=autor_answer_data['title'], autor_answer_data=autor_answer_data)
+
+
+class LoginForm(FlaskForm):
+    id_astronaut = StringField('id астронавта', validators=[DataRequired()])
+    password_astronaut = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    id_capitan = StringField('id капитана', validators=[DataRequired()])
+    password_capitan = PasswordField('Пароль капитана', validators=[DataRequired()])
+
+    submit = SubmitField('Доступ')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
