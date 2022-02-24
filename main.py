7eb823c6979b476adb9d0d19609eddbe688f5d8c
kath <katya.permyakova.2004@mail.ru>
2022-02-24 10:40:26 +0300

    из урока

--- main.py
+++ main.py
@@ -1,39 +1,63 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 from data import db_session
 from data.users import User
+from data.news import News
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 def main():
     db_session.global_init("db/blogs.sqlite")
-    # app.run()
 
-    user = User()
-    user.surname = 'Scott'
-    user.name = "Ridley"
-
-    user.about = "биография пользователя 1"
-    user.email = "email@email.ru"
+    new = False
     db_sess = db_session.create_session()
-    db_sess.add(user)
+    if new:
+        for i in range(3):
+            user = User()
+            user.name = f"Пользователь {i}"
+            user.about = f"биография пользователя {i}"
+            user.email = f"email{str(i) * 5}@email.ru"
+            db_sess.add(user)
     db_sess.commit()
 
+    for new in db_sess.query(News).all():
+        print(new.title)
 
-    # new = False
-    # db_sess = db_session.create_session()
-    # if new:
-    #     for i in range(3):
-    #         user = User()
-    #         user.name = f"Пользователь {i}"
-    #         user.about = f"биография пользователя {i}"
-    #         user.email = f"email{str(i) * 5}@email.ru"
-    #         db_sess.add(user)
-    # db_sess.commit()
-    #
-    # for user in db_sess.query(User).all():
-    #     print(user.name)
+    app.run()
 
 
 if __name__ == '__main__':
