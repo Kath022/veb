3203aaf47755fefcea96c79486b0ec21c9b8f396
kath <katya.permyakova.2004@mail.ru>
2022-03-02 22:00:32 +0300

    Тестирование ответа сервера2

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -1,5 +1,5 @@
 import flask
-
+from flask import request, jsonify
 from . import db_session
 from .job import Jobs
 
@@ -24,6 +24,33 @@ def get_news():
                     for item in jobs]
         }
     )
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_job():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ['id', 'team_leader', 'job',
+                  'work_size', 'collaborators', 'is_finished']):
+        return jsonify({'error': 'Bad request'})
+
+    db_sess = db_session.create_session()
+    exist_job = db_sess.query(Jobs).filter(Jobs.id == request.json['id']).first()
+    if exist_job is not None:
+        return jsonify({'error': 'Id already exists'})
+
+    job = Jobs(
+        id=request.json['id'],
+        team_leader=request.json['team_leader'],
+        job=request.json['job'],
+        work_size=request.json['work_size'],
+        collaborators=request.json['collaborators'],
+        is_finished=request.json['is_finished'],
+    )
+    db_sess.add(job)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
 
 
 @blueprint.route('/api/jobs/<int:job_id>', methods=['GET'])
--- test.py
+++ test.py
@@ -1,7 +1,30 @@
-from requests import get
+from requests import get, post
 
-print(get('http://localhost:5000/api/jobs').json())
+# пустой запрос
+print(post('http://localhost:5000/api/jobs').json())
+# некоректный запрос
+print(post('http://localhost:5000/api/jobs', json={
+               'id': '8',
+               'team_leader': '1',
+               'job': 'rere',
+               'work_size': 2,}).json())
 
-print(get('http://localhost:5000/api/jobs/1').json())
-print(get('http://localhost:5000/api/jobs/99999').json())
-print(get('http://localhost:5000/api/jobs/qw').json())
+# уже есть индекст
+print(post('http://localhost:5000/api/jobs',
+           json={
+               'id': '1',
+               'team_leader': '1',
+               'job': 'rere',
+               'work_size': 2,
+               'collaborators': '2, 1',
+               'is_finished': True}).json())
+
+# корректно
+print(post('http://localhost:5000/api/jobs',
+           json={
+               'id': '13',
+               'team_leader': '1',
+               'job': 'rere',
+               'work_size': 2,
+               'collaborators': '2, 1',
+               'is_finished': True}).json())
